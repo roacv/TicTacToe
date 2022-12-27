3582e9f1f8c3a9e6676438a5fb40a00df4f23b4d
roacv <roacv@hotmail.com>
2022-12-27 15:52:02 +0000

    V final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
-// Aqui va mi elemento apolleta que lo recibiremos con los parámetros para que ponga color y toda la volá
 const Ampolleta = () =>{
-  const [className, setClassState] = useState('yellow');
-  const cambiarColor = () => {
-    let cS = className;
-    console.log(className);
-    cS = cS.includes("red") ? "on" : "red";
-    console.log(cS);
-    setClassState(cS);
+  const [color, setColortate] = useState('red');
+  const cambiarColor = (activar) => {
+    // console.log(activar);
+    // console.log(color)
+    setColortate(activar);
   };
   return(
     <>
-      <div onClick={cambiarColor} className={className+" red "}></div>
-      <div onClick={cambiarColor} className={className+" yellow "}></div>
-      <div onClick={cambiarColor} className={className+" green "}></div>
+      <div className={"red "+(color==="red" ? "on" : "")} onClick={()=>cambiarColor('red')} ></div>
+      <div className={"yellow "+(color==="yellow" ? "on" : "")} onClick={()=>cambiarColor('yellow')} ></div>
+      <div className={"green " +(color==="green" ? "on" : "")} onClick={()=>cambiarColor('green')} ></div>
     </>
   );
 }
-
 
 function App() {
 
@@ -28,9 +23,7 @@ function App() {
     <>
       <div className='poste mx-auto'></div>
       <div className='container py-3'>
-        <Ampolleta/>
-        {/* <Ampolleta className={"yellow"}/>
-        <Ampolleta className={"green"}/> */}
+        <Ampolleta />
       </div>
     </>
   );
